7777d8f00e5afda96a0f1f2e01f96f03d01cbbf3
twillzy <william.tanzy@gmail.com>
2017-06-01 00:16:38 +1000

    instagram instead of twitter for pfc

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -40,8 +40,8 @@ twillzy =
 
 prisonerFatCat : Blogger
 prisonerFatCat =
-    { name = "Pris"
-    , occupation = "Tech lead"
+    { name = "Pris The Cat"
+    , occupation = "Chief Cat Officer"
     , company = "Google"
     , profilePic = "static/img/pris.jpg"
     , githubUrl = "#"
@@ -73,25 +73,32 @@ update msg blogger =
 
 view : Blogger -> Html Msg
 view blogger =
-    div [ class "container", style [ ( "text-align", "center" ) ] ]
-        [ ul [ class "nav nav-pills navbar-right" ]
-            [ li [ attribute "role" "presentation" ]
-                [ a [ href blogger.githubUrl ]
-                    [ text "Github" ]
+    let
+        socialMedia =
+            if blogger == twillzy then
+                "Twitter"
+            else
+                "Instagram"
+    in
+        div [ class "container", style [ ( "text-align", "center" ) ] ]
+            [ ul [ class "nav nav-pills navbar-right" ]
+                [ li [ attribute "role" "presentation" ]
+                    [ a [ href blogger.githubUrl ]
+                        [ text "Github" ]
+                    ]
+                , li [ attribute "role" "presentation" ]
+                    [ a [ href blogger.twitterUrl ]
+                        [ text socialMedia ]
+                    ]
                 ]
-            , li [ attribute "role" "presentation" ]
-                [ a [ href blogger.twitterUrl ]
-                    [ text "Twitter" ]
+            , div [ class "row" ]
+                [ div [ class "col-xs-12" ]
+                    [ img [ src blogger.profilePic, class "img-circle", style styles.img, onClick ToggleImage ] []
+                    , h2 [] [ text blogger.name ]
+                    , p [ class "lead" ] [ text (blogger.occupation ++ " at " ++ blogger.company) ]
+                    ]
                 ]
             ]
-        , div [ class "row" ]
-            [ div [ class "col-xs-12" ]
-                [ img [ src blogger.profilePic, class "img-circle", style styles.img, onClick ToggleImage ] []
-                , h2 [] [ text blogger.name ]
-                , p [ class "lead" ] [ text (blogger.occupation ++ " at " ++ blogger.company) ]
-                ]
-            ]
-        ]
 
 
 
